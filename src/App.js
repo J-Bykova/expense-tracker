1ed2b92cb64fe3518605cda2d5bc41ca6d6ca391
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-07-22 14:41:06 -0700

    Create IncomeExpense

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import './App.css';
 import {Header} from './components/Header'
 import {Balance} from './components/Balance'
+import {IncomeExpense} from './components/IncomeExpense'
 
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Header/>
       <div className='container'>
         <Balance />
+        <IncomeExpense />
       </div>
     </div>
   );
