c89060d646018fa68ced4eb0a54baeb98430d84c
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-07-22 14:52:54 -0700

    Create AddTransaction and delete id

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Header} from './components/Header'
 import {Balance} from './components/Balance'
 import {IncomeExpense} from './components/IncomeExpense'
 import {TransactionList} from './components/TransactionList'
+import {AddTransaction} from './components/AddTransaction'
 
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
         <Balance />
         <IncomeExpense />
         <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
