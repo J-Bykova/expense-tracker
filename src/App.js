5d8cdc4fc39a51a890b31514d931e8b2f93a6ce8
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-07-22 14:47:36 -0700

    Create TransactionList

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {Header} from './components/Header'
 import {Balance} from './components/Balance'
 import {IncomeExpense} from './components/IncomeExpense'
+import {TransactionList} from './components/TransactionList'
 
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
       <div className='container'>
         <Balance />
         <IncomeExpense />
+        <TransactionList />
       </div>
     </div>
   );
